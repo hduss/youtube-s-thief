324f58f70f19399e73cfcbc8db7c0f6c8c332ab7
hduss <maintenancedotnet3@gmail.com>
2021-12-14 01:00:59 +0100

    add start program && while loop for next pages

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # API client library
 import os
 import pickle
-import json
 import time
 import title
-
+import colorama
 import google.oauth2.credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
+colorama.init()
 
-def get_credentials():
+
+def authenticate_youtube():
     # API information
     # API_KEY = "AIzaSyAYheEHWuVrMYXn4C4JKSv3OKMGUku73a4"
     api_service_name = "youtube"
@@ -36,35 +37,68 @@ def get_credentials():
         with open("token.pickle", "wb") as token:
             pickle.dump(credentials, token)
 
-    # Display credentials
-    # print(credentials.to_json())
-
     youtube = build(api_service_name, api_version, credentials=credentials)
     return youtube
 
 
 def get_videos(build):
-    
     request = build.videos().list(
         part='contentDetails, snippet',
         myRating='like',
-        maxResults=10
+        maxResults=50
     )
-    
+
+    resp = request.execute()  # Query execution
+    return resp
+
+
+def get_video_next_page(build, next_page_token):
+    request = build.videos().list(
+        part='contentDetails, snippet',
+        myRating='like',
+        maxResults=50,
+        pageToken=next_page_token
+    )
+
     resp = request.execute()  # Query execution
     return resp
 
 
 def main():
+
     title.print_title()
-    
-    build = get_credentials()  # get credentials
-    response = get_videos(build)  # get videos
-    
-    # print(json.dumps(response, indent=2))  # Print results
-    for item in response['items']:
-        time.sleep(0.02)
-        print("[+] " + item['snippet']['title'] + " - " + item['id'])
+    nbr_pages = title.start_program()
+
+    if nbr_pages > 0:
+
+        build = authenticate_youtube()  # get credentials
+        response = get_videos(build)  # get videos
+        next_page_token = response['nextPageToken']
+
+        print(colorama.Fore.CYAN + colorama.Style.BRIGHT + colorama.Back.WHITE + '=== page 1' + colorama.Style.RESET_ALL)
+        # print(json.dumps(response, indent=2))  # Print results
+        for item in response['items']:
+            time.sleep(0.03)
+
+            print(colorama.Fore.YELLOW + colorama.Style.NORMAL + "[+] " + item['snippet']['title'] + " - " + item[
+                'id'] + colorama.Style.RESET_ALL)
+
+        i = 1  # For DEBUG
+        # While loop for next page | get_video_next_page() function
+        while next_page_token and i < nbr_pages:
+            r = get_video_next_page(build, next_page_token)
+            next_page_token = r['nextPageToken']
+            print("Next Page Token while => " + next_page_token)
+            for item in r['items']:
+                time.sleep(0.03)
+
+                print(colorama.Fore.YELLOW + colorama.Style.NORMAL + "[+] " + item['snippet']['title'] + " --- " + item[
+                    'id'] + colorama.Style.RESET_ALL)
+
+            i += 1  # For DEBUG
+
+    else:
+        print('Aucune vidéo n\' été téléchargé')
 
 
 if __name__ == "__main__":
--- title.py
+++ title.py
@@ -1,9 +1,24 @@
+import colorama
+
+colorama.init()
+
+
+def start_program():
+    print('Une page Contient 50 vidéos')
+    nbr_pages = input("Combien de pages souhaitez-vous télécharger ? ")
+    return int(nbr_pages)
+
 
 def print_title():
-    print("                           __             __                  __     __      _           ____")
-    print("   __  __  ____   __  __  / /_  __  __   / /_   ___          / /_   / /_    (_)  ___    / __/")
-    print("  / / / / / __ \ / / / / / __/ / / / /  / __ \ / _ \        / __/  / __ \  / /  / _ \  / /_  ")
-    print(" / /_/ / / /_/ // /_/ / / /_  / /_/ /  / /_/ //  __/       / /_   / / / / / /  /  __/ / __/  ")
-    print(" \__, /  \____/ \__,_/  \__/  \__,_/  /_.___/ \___/        \__/  /_/ /_/ /_/   \___/ /_/     ")
-    print("/____/  ")
+    print(colorama.Fore.RED + colorama.Style.NORMAL)
+    print("                         __          __         _          __   __     _        ____")
+    print("   __  __ ____   __  __ / /_ __  __ / /_   ___ ( )_____   / /_ / /_   (_)___   / __/")
+    print("  / / / // __ \ / / / // __// / / // __ \ / _ \|// ___/  / __// __ \ / // _ \ / /_  ")
+    print(" / /_/ // /_/ // /_/ // /_ / /_/ // /_/ //  __/ (__  )  / /_ / / / // //  __// __/  ")
+    print(" \__, / \____/ \__,_/ \__/ \__,_//_.___/ \___/ /____/   \__//_/ /_//_/ \___//_/     ")
+    print("/____/ ")
+
+    print(colorama.Style.RESET_ALL)
+
+
 
